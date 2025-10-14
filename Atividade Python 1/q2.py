2b8cb5465acde5a2f9662933930f9d75f2295488
Fábio Júnior <fabiojnr.dev@gmail.com>
2025-10-14 18:52:13 -0300

    Update q2.py

--- Atividade Python 1/q2.py
+++ Atividade Python 1/q2.py
@@ -6,8 +6,9 @@ saldo = 1500.758'''
 
 nome = "João"
 saldo = 1500.758
-# Saida esperada: Olá, João. Seu saldo é 1500.76.
+'''Saida esperada: Olá, João. Seu saldo é 1500.76.'''
 print(f"Olá, {nome}. Seu saldo é {saldo:.2f}.")
-# Utilizada a f-string para formatar a saída, limitando o saldo a duas casas decimais.
-# A f-string foi usada também para inserir a variável nome diretamente na string.
-# Por conta de tudo isso, a saída ficou exatamente como esperado.
+'''Utilizada a f-string para formatar a saída, limitando o saldo a duas casas decimais.
+A f-string foi usada também para inserir a variável nome diretamente na string.
+Por conta de tudo isso, a saída ficou exatamente como esperado.'''
+
