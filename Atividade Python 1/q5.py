c3ce32f01c311e8d8dd25a7c5bc0419d9813ce34
Fábio Júnior <fabiojnr.dev@gmail.com>
2025-10-14 18:50:35 -0300

    Update q5.py

--- Atividade Python 1/q5.py
+++ Atividade Python 1/q5.py
@@ -1,9 +1,9 @@
-# Q5: Determine o valor final da variável check
+''' Q5: Determine o valor final da variável check'''
 a = 10
 b = 5
 c = 15
 check = (a > b and c < b) or not (c==15)
 print(check)
-# A saída será False por conta das expressões lógicas.
-# A expressão (a > b and c < b) é False porque c não é menor que b.
-# A expressão not (c==15) é False porque c é igual a 15.
+'''A saída será False por conta das expressões lógicas.
+A expressão (a > b and c < b) é False porque c não é menor que b.
+A expressão not (c==15) é False porque c é igual a 15.'''
